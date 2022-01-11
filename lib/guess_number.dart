879a4f3beea94c49a29ab69f1cce6243590c9a00
Bubparam <95558278+Bubparam@users.noreply.github.com>
2022-01-11 19:57:57 +0700

    Update guess_number.dart

--- lib/guess_number.dart
+++ lib/guess_number.dart
@@ -4,16 +4,34 @@ import 'dart:io';
 import 'package:guess_number/game.dart';
 
 void main() {
-    stdout.write(' Maximum Number To Random : ');
-    var num = stdin.readLineSync();
-    var max = int.tryParse(num!);
-    max ??= 100;
-    var game = Game(maxRandom: max);
-    print('╔════════════════════════════════════════');
-    print('║            GUESS THE NUMBER            ');
-    print('╟────────────────────────────────────────');
+  while (true) {
+    playGame();
+    String? input;
+    do {
+      stdout.write('Play again? (Y/N): ');
+      input = stdin.readLineSync();
+    } while (input!.toLowerCase() != 'y' && input.toLowerCase() != 'n');
 
-  while(true) {
+    if (input.toLowerCase() == 'n') break;
+  }
+
+  print('\n\nYou\'ve played ${Game.myList.length} games');
+  for (var i = 0; i < Game.myList.length; i++) {
+    print('Game #${i + 1} : ${Game.myList[i]} guesses');
+  }
+}
+
+void playGame() {
+  stdout.write(' Maximum Number To Random : ');
+  var num = stdin.readLineSync();
+  var max = int.tryParse(num!);
+  max ??= 100;
+  var game = Game(maxRandom: max);
+  print('╔════════════════════════════════════════');
+  print('║            GUESS THE NUMBER            ');
+  print('╟────────────────────────────────────────');
+
+  while (true) {
     stdout.write('║ Guess the number between 1 and $max : ');
     var input = stdin.readLineSync();
     var guess = int.tryParse(input!);
@@ -34,18 +52,7 @@ void main() {
       break;
     }
   }
-    print('║                 THE END                ');
-    print('╚════════════════════════════════════════');
-    stdout.write(' Play Again? (Y/N): ');
-    if(game.playAgain()==true) {
-      main();
-    }else if(Game.myList.length == 1){
-      print(' You Have Played : ${Game.myList.length} game');
-      print(' Game #1 : ${Game.myList[0]} guesses');
-    }else{
-      print(' You Have Played : ${Game.myList.length} games');
-      for(int i=0; i<Game.myList.length; i++){
-        print(' Game #${i+1} : ${Game.myList[i]} guesses');
-      }
-    }
-  }
+  print('║                 THE END                ');
+  print('╚════════════════════════════════════════');
+  Game.myList.add(game.count);
+}
