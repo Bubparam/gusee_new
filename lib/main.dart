21eab8b0117c23320f9dd2512d11f8eb71c32172
Bubparam <95558278+Bubparam@users.noreply.github.com>
2022-01-11 19:57:25 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:guess_number/game.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +21,9 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
+  var game = Game(maxRandom: 100);
+  final _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -46,37 +49,135 @@ class HomePage extends StatelessWidget {
                 mainAxisSize: MainAxisSize.min,
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset('assets/images/guess_logo.png',
-                          height: 300, width: 200),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Guess',
-                            style: TextStyle(
-                                fontSize: 50.0, color: Color(0x80FF0000)),
-                          ),
-                          Text(
-                            'The Number',
-                            style: TextStyle(
-                                fontSize: 30.0, color: Color(0x80FF0000)),
-                          ),
-                        ],
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset('assets/images/guess_logo.png',
+                            height: 300, width: 200),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Guess',
+                              style: TextStyle(
+                                  fontSize: 50.0, color: Color(0x80FF0000)),
+                            ),
+                            Text(
+                              'The Number',
+                              style: TextStyle(
+                                  fontSize: 25.0, color: Color(0x80FF0000)),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: TextField(
+                      textAlign: TextAlign.center,
+                      controller: _controller,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white.withOpacity(0.7),
+                        border: OutlineInputBorder(),
+                        hintText: 'ทายเลขตั้งแต่ 1 ถึง 100',
                       ),
-                    ],
+                    ),
                   ),
-                  TextField(),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  TextButton(
-                    child: Text('Guess'),
-                    style: TextButton.styleFrom(
-                        primary: Colors.white, backgroundColor: Colors.pink),
-                    onPressed: () {},
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: TextButton(
+                      child: Text('Guess'),
+                      style: TextButton.styleFrom(
+                          primary: Colors.white, backgroundColor: Colors.pink),
+                      onPressed: () {
+                        var input = _controller.text;
+                        var guess = int.tryParse(input!);
+                        if (guess == null) {
+                          showDialog(
+                              context: context,
+                              barrierDismissible: false,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text('Error'),
+                                  content: Text(
+                                      'กรอกข้อมูลไม่ถูกต้อง กรุณากรอกใหม่'),
+                                  actions: [
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: Text('OK'),
+                                    )
+                                  ],
+                                );
+                              });
+                        }
+                        var result = game.doGuess(guess!);
+                        if (result == 1) {
+                          showDialog(
+                              context: context,
+                              barrierDismissible: false,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text('Result'),
+                                  content:
+                                      Text('$guess มากเกินไป กรุณาลองใหม่'),
+                                  actions: [
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: Text('OK'),
+                                    )
+                                  ],
+                                );
+                              });
+                        }else if(result == -1){
+                          showDialog(
+                              context: context,
+                              barrierDismissible: false,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text('Result'),
+                                  content:
+                                  Text('$guess น้อยเกินไป กรุณาลองใหม่'),
+                                  actions: [
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: Text('OK'),
+                                    )
+                                  ],
+                                );
+                              });
+                        }else{
+                          showDialog(
+                              context: context,
+                              barrierDismissible: false,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text('Result'),
+                                  content:
+                                  Text('$guess ถูกต้องครับ คุณทายทั้งหมด ${game.count} ครั้ง'),
+                                  actions: [
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: Text('OK'),
+                                    )
+                                  ],
+                                );
+                              });
+                        }
+                      },
+                    ),
                   ),
                 ],
               ),
